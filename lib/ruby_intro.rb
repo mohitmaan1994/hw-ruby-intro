44ab7990e634f921933dd3d948cb06e879c4b493
EC2 Default User <ec2-user@ip-172-31-67-70.ec2.internal>
2020-09-08 03:36:30 +0000

    assignment HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,67 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+ if arr.empty?
+    return false if n == 0
+ else
+    arr.combination(2).to_a.each do |pair|
+      return true if sum(pair) == n
+    end
+ end
+    return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  st = "Hello, "+ name
+  return st
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.empty?
+  	return false
+  elsif ( /\A[qwrtypsdfghjklzxcvbnm]{1}/i =~ s[0] )
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
